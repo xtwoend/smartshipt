8c0d951b1ad127925171b5817d27ab8a32f3debe
Abdul Hafidz A <aditans88@gmail.com>
2023-01-13 18:04:37 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,7 +55,7 @@
                             <div class="input-group input-group-flat">
                                 <input name="password" type="password" class="form-control"  placeholder="Your password"  autocomplete="off">
                                 <span class="input-group-text">
-                                    <a id="see" href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+                                    <a id="see" href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
                                     </a>
                                 </span>
@@ -80,12 +80,5 @@
             </div>
         </div>
     </div>
-    <script>
-        $(document).ready(function(){
-            $('#see').click(function(){
-                alert('adf');
-            });
-        });
-    </script>
 </body>
 </html>
