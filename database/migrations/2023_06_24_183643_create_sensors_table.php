4a5c7efde0251e5bb70cf2d156563536a2ad0c3f
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-06-24 18:37:54 +0700

    update

---
+++ database/migrations/2023_06_24_183643_create_sensors_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fleet_id');
+            $table->string('fleet_type')->nullable();
+            $table->string('group');
+            $table->string('name');
+            $table->string('sensor_name');
+            $table->string('unit')->nullable();
+            $table->float('min', 15, 5)->default(0);
+            $table->float('max', 15, 5)->default(0);
+            $table->float('warning', 15, 5)->default(0);
+            $table->float('danger', 15, 5)->default(0);
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+};
