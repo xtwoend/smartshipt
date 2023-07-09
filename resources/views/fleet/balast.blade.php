81d5677f48e5118b47ebba41174f9cb6ef7aaf7d
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-07-09 14:28:41 +0700

    update

--- resources/views/fleet/balast.blade.php
+++ resources/views/fleet/balast.blade.php
@@ -6,6 +6,11 @@
         <slider-submenu :fleet="{{ json_encode($fleet) }}" active="balast"></slider-submenu>
         <div class="container">
             <fleet-ballast url="{{ route('api.fleet', $fleet->id) }}"></fleet-ballast>
+
+            <trend-view 
+                    title="Trend View Ballast"
+                    url="{{ route('api.fleet.ballast.trend', $fleet->id) }}"
+                    :columns="{{ json_encode($fleet->trendOptions('ballast')) }}"></trend-view>
         </div>
     </div>
 </main>
