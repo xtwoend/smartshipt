1e0521de95bbcdd122f46402d233ef68fc2f2a54
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-07-10 07:56:35 +0700

    update

--- resources/views/fleet/cargo.blade.php
+++ resources/views/fleet/cargo.blade.php
@@ -5,14 +5,14 @@
     <div class="bg-white">
         <slider-submenu :fleet="{{ json_encode($fleet) }}" active="cargo"></slider-submenu>
 
-        <div class="container">
+        {{-- <div class="container"> --}}
             <fleet-cargo url="{{ route('api.fleet', $fleet->id) }}"></fleet-cargo>
 
             <trend-view 
                     title="Trend View Cargo"
                     url="{{ route('api.fleet.cargo.trend', $fleet->id) }}"
                     :columns="{{ json_encode($fleet->trendOptions('cargo')) }}"></trend-view>
-        </div>
+        {{-- </div> --}}
     </div>
 </main>
 @endsection
