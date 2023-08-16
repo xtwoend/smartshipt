09694d0d73f1de6acfda125ddec6ab50d1c6f934
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-08-16 23:24:41 +0700

    update

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -22,4 +22,24 @@ class Cargo extends Model
 
         return $model->setTable($tableName);
     }
+
+    public function information() 
+    {
+        $informations = [];
+        foreach(Sensor::where('fleet_id', $this->fleet_id)->whereGroup('cargo')->orderBy('id')->get() as $limit) {
+            $diff = Carbon::parse($this->updated_at)->diffInMinutes(Carbon::now());
+            $value = $diff < 10 ?  $this->{$limit->sensor_name} : 0;
+            $informations[$limit->id] = [
+                'title' => $limit->name,
+                'value' => $value,
+                'unit' => $limit->unit,
+                'min' => $limit->min,
+                'normal' => $limit->normal,
+                'danger' => $limit->danger,
+                'max' => $limit->max,
+            ];
+        }
+
+        return (array) $informations;
+    }
 }
