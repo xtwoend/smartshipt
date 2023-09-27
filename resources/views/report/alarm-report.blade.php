1da10870f4517417fe3728076c4e5432728bea83
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-09-27 15:18:10 +0700

    update

--- resources/views/report/alarm-report.blade.php
+++ resources/views/report/alarm-report.blade.php
@@ -138,7 +138,7 @@
             </td>
           </tr>
           <tr class="row1">
-            <td class="column1 style11 s style11" colspan="3">ALARM REPORT DAILY</td>
+            <td class="column1 style11 s style11" colspan="3">DAILY ALARM REPORT</td>
           </tr>
           <tr class="row2">
             <td class="column1 style14 s style14" colspan="3">Smartship System - Fleet Management Solution</td>
