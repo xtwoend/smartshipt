06f5adf9c432772901ae16f0fb43fa2bdb00c6ed
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-07 21:39:58 +0700

    update

--- resources/views/fleet/_menu.blade.php
+++ resources/views/fleet/_menu.blade.php
@@ -6,25 +6,25 @@ $active = request()->route()->getName();
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav">
             <li class="nav-item {{ $active == 'fleet' ? 'active': '' }}">
-                <a class="nav-link" href="/">NAVIGASI</a>
+                <a class="nav-link" href="{{ route('fleet') }}">NAVIGASI</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.engine' ? 'active': '' }}">
-                <a class="nav-link" href="/">MAIN ENGINE</a>
+                <a class="nav-link" href="{{ route('fleet.engine') }}">MAIN ENGINE</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.pumps' ? 'active': '' }}">
-                <a class="nav-link" href="/">PUMPS</a>
+                <a class="nav-link" href="{{ route('fleet.pumps') }}">PUMPS</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.cargo' ? 'active': '' }}">
-                <a class="nav-link" href="/">CARGO TANK</a>
+                <a class="nav-link" href="{{ route('fleet.cargo') }}">CARGO TANK</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.bunker' ? 'active': '' }}">
-                <a class="nav-link" href="/">BUNKER TANK</a>
+                <a class="nav-link" href="{{ route('fleet.bunker') }}">BUNKER TANK</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.balast' ? 'active': '' }}">
-                <a class="nav-link" href="/">BALAST TANK</a>
+                <a class="nav-link" href="{{ route('fleet.balast') }}">BALAST TANK</a>
             </li>
             <li class="nav-item {{ $active == 'fleet.alarms' ? 'active': '' }}">
-                <a class="nav-link" href="/">ALARM</a>
+                <a class="nav-link" href="{{ route('fleet.alarms') }}">ALARM</a>
             </li>
         </ul>
     </div>
