f71be9b0f368e75576546f8c0e574b3302fe660f
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-08 08:18:33 +0700

    update

--- app/Http/Controllers/Master/SensorController.php
+++ app/Http/Controllers/Master/SensorController.php
@@ -10,10 +10,13 @@ class SensorController extends Controller
 {
     public function update(Request $request)
     {
+        $input = $request->all();
+        $input['is_ams'] = ($request->is_ams == 'True') ? 1 : 0;
+        
         if($request->has('id') && $request->id !== 0) {
-            $sensor = Sensor::find($request->id)->update($request->all());
+            $sensor = Sensor::find($request->id)->update($input);
         }else{
-            $sensor = Sensor::create($request->all());
+            $sensor = Sensor::create($input);
         }
         return $sensor;
     }
