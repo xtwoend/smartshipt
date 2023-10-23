c604d520b8bc11b8149fa34b3c28aeab3a4e10cc
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-23 10:14:09 +0700

    update

--- config/defines.php
+++ config/defines.php
@@ -12,6 +12,7 @@ return [
         8 => 'rpm_propeller',
         10 => 'me_rpm',
         11 => 'me_rpm',
-        20 => 'me_rev_speed_con_value'
+        20 => 'me_rev_speed_con_value',
+        22 => 'engine_speed',
     ]
 ];
