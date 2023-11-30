737929f093eb5b3913915400f0685a5f4414d819
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-11-30 15:04:52 +0700

    update

--- app/functions.php
+++ app/functions.php
@@ -65,6 +65,6 @@ if( ! function_exists('secondsToTime')) {
     function secondsToTime($seconds) {
         $dtF = new \DateTime('@0');
         $dtT = new \DateTime("@$seconds");
-        return $dtF->diff($dtT)->format('%a d, %h h, %i m, %s s');
+        return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
     }
 }
