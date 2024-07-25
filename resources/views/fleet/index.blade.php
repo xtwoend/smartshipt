f3baedbc99a6b7670246a7d813825f2722dbc077
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-07-25 10:11:16 +0700

    update

--- resources/views/fleet/index.blade.php
+++ resources/views/fleet/index.blade.php
@@ -10,8 +10,8 @@
         {{-- <slider-submenu :fleet="{{ json_encode($fleet) }}" active="balast"></slider-submenu> --}}
     @endif
     <map-default :fleet="{{ json_encode($fleet) }}" style="height: 450px; width:100%;"></map-default>
-    <fleet-information url="{{ route('api.fleet', $fleet->id) }}"></fleet-information>
-    {{-- <data-info url="{{ route('api.fleet.nav.current', $fleet->id) }}" :mapping="{{ json_encode($fleet->trendOptions('navigation')) }}"></data-info> --}}
+    {{-- <fleet-information url="{{ route('api.fleet', $fleet->id) }}"></fleet-information> --}}
+    <data-info url="{{ route('api.fleet.nav.current', $fleet->id) }}" :mapping="{{ json_encode($fleet->trendOptions('navigation')) }}"></data-info>
     <trend-live 
         title="Trend Live Navigation"
         url="{{ route('api.fleet.nav.current', $fleet->id) }}"
