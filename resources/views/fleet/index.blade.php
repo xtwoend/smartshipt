91885f2d851f0327a09a35b08caeec652d117dd1
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-07-25 10:09:16 +0700

    update

--- resources/views/fleet/index.blade.php
+++ resources/views/fleet/index.blade.php
@@ -10,14 +10,29 @@
         {{-- <slider-submenu :fleet="{{ json_encode($fleet) }}" active="balast"></slider-submenu> --}}
     @endif
     <map-default :fleet="{{ json_encode($fleet) }}" style="height: 450px; width:100%;"></map-default>
-    <fleet-information url="{{ route('api.fleet', $fleet->id) }}"></fleet-information>
-    <div class="p-3">
+    {{-- <fleet-information url="{{ route('api.fleet', $fleet->id) }}"></fleet-information> --}}
+    <data-info url="{{ route('api.fleet.nav.current', $fleet->id) }}" :mapping="{{ json_encode($fleet->trendOptions('navigation')) }}"></data-info>
+    <trend-live 
+        title="Trend Live Navigation"
+        url="{{ route('api.fleet.nav.current', $fleet->id) }}"
+        {{-- url="{{ route('api.fleet.logger', ['id' => $fleet->id, 'group'=> 'cargo']) }}"  --}}
+        :fleet="{{ json_encode($fleet) }}"
+        :socket-config="{ url: '{{ config('websocket.url') }}', event: 'cargo_{{ $fleet->id }}'}"
+        :columns="{{ json_encode($fleet->trendOptions('navigation')) }}">
+    </trend-live>
+
+    <trend-view 
+            title="Trend View Navigation"
+            url="{{ route('api.fleet.nav.trend', $fleet->id) }}"
+            :columns="{{ json_encode($fleet->trendOptions('navigation')) }}"></trend-view>
+    </div>
+    {{-- <div class="p-3">
         <div class="row">
             <div class="col">
                 <trend-live 
                     title="Trend Live"
                     url="{{ route('api.fleet.nav.current', $fleet->id) }}"
-                    {{-- url="{{ route('api.fleet.logger', ['id' => $fleet->id, 'group'=> 'navigation']) }}"  --}}
+                    url="{{ route('api.fleet.logger', ['id' => $fleet->id, 'group'=> 'navigation']) }}" 
                     :fleet="{{ json_encode($fleet) }}"
                     :socket-config="{ url: '{{ config('websocket.url') }}', event: 'navigation_{{ $fleet->id }}'}"
                     :columns="{{ json_encode([
@@ -107,7 +122,7 @@
                 </trend-view>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 </main>
 @endsection
