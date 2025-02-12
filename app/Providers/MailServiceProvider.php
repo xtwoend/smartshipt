919a73bf8bfbdf016f2494f7327391eb39f94751
Cheetah <Cheetah@localhost>
2025-02-12 16:18:11 +0700

    init cargo_calc

--- app/Providers/MailServiceProvider.php
+++ app/Providers/MailServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use stdClass;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use stdClass;
 
 class MailServiceProvider extends ServiceProvider
 {
@@ -17,31 +18,33 @@ class MailServiceProvider extends ServiceProvider
     public function register()
     {
         // config email from db
-        $all = DB::table('settings')->where('group', 'email')->get();
-        if(count($all) > 0) {
-            $mail = new stdClass;
-            foreach($all as $m) {
-                $mail->{$m->key} = $m->value;
-            }
+        if (Schema::hasTable('settings')) {
+            $all = DB::table('settings')->where('group', 'email')->get();
+            if (count($all) > 0) {
+                $mail = new stdClass;
+                foreach ($all as $m) {
+                    $mail->{$m->key} = $m->value;
+                }
 
-            $config = array(
-                'from' => array('address' => $mail->smtp_from_address, 'name' => $mail->smtp_from_name),
-                'mailers' => [
-                    'smtp' => [
-                        'transport' => 'smtp',
-                        'host'       => $mail->smtp_host,
-                        'port'       => $mail->smtp_port,
-                        'encryption' => $mail->smtp_encryption ?? 'tls',
-                        'username'   => $mail->smtp_username,
-                        'password'   => $mail->smtp_password,
-                        'timeout' => null,
-                        'local_domain' => env('MAIL_EHLO_DOMAIN'),
+                $config = array(
+                    'from' => array('address' => $mail->smtp_from_address, 'name' => $mail->smtp_from_name),
+                    'mailers' => [
+                        'smtp' => [
+                            'transport' => 'smtp',
+                            'host'       => $mail->smtp_host,
+                            'port'       => $mail->smtp_port,
+                            'encryption' => $mail->smtp_encryption ?? 'tls',
+                            'username'   => $mail->smtp_username,
+                            'password'   => $mail->smtp_password,
+                            'timeout' => null,
+                            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+                        ]
                     ]
-                ]
-            );
+                );
 
-            Config::set('mail.mailers', $config['mailers']);
-            Config::set('mail.from', $config['from']);
+                Config::set('mail.mailers', $config['mailers']);
+                Config::set('mail.from', $config['from']);
+            }
         }
     }
 
@@ -50,8 +53,5 @@ class MailServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        
-    }
+    public function boot() {}
 }
--- database/migrations/2023_05_30_175823_update_fleets_table_add_info.php
+++ database/migrations/2023_05_30_175823_update_fleets_table_add_info.php
@@ -14,18 +14,18 @@ return new class extends Migration
     public function up()
     {
         Schema::table('fleets', function(Blueprint $table){
-            $table->double('swl', 10, 3)->default(0)->after('flag');
-            $table->double('draft', 10, 3)->default(0)->after('flag');
-            $table->double('lwt', 10, 3)->default(0)->after('flag');
-            $table->double('nrt', 10, 3)->default(0)->after('flag');
-            $table->double('grt', 10, 3)->default(0)->after('flag');
-            $table->double('dwt', 10, 3)->default(0)->after('flag');
-            $table->double('death', 10, 3)->default(0)->after('flag');
-            $table->double('breadth', 10, 3)->default(0)->after('flag');
-            $table->double('length', 10, 3)->default(0)->after('flag');
-            $table->string('mmsi', 255)->nullable()->after('flag');
-            $table->string('class', 255)->nullable()->after('flag');
-            $table->string('home_port', 255)->nullable()->after('flag');
+            // $table->double('swl', 10, 3)->default(0)->after('flag');
+            // $table->double('draft', 10, 3)->default(0)->after('flag');
+            // $table->double('lwt', 10, 3)->default(0)->after('flag');
+            // $table->double('nrt', 10, 3)->default(0)->after('flag');
+            // $table->double('grt', 10, 3)->default(0)->after('flag');
+            // $table->double('dwt', 10, 3)->default(0)->after('flag');
+            // $table->double('death', 10, 3)->default(0)->after('flag');
+            // $table->double('breadth', 10, 3)->default(0)->after('flag');
+            // $table->double('length', 10, 3)->default(0)->after('flag');
+            // $table->string('mmsi', 255)->nullable()->after('flag');
+            // $table->string('class', 255)->nullable()->after('flag');
+            // $table->string('home_port', 255)->nullable()->after('flag');
         });
     }
 
@@ -37,20 +37,20 @@ return new class extends Migration
     public function down()
     {
         Schema::table('fleets', function(Blueprint $table){
-            $table->dropColumn([
-                'swl',
-                'draft',
-                'lwt',
-                'nrt',
-                'grt',
-                'dwt',
-                'death',
-                'breadth',
-                'length',
-                'mmsi',
-                'class',
-                'home_port'
-            ]);
+            // $table->dropColumn([
+            //     'swl',
+            //     'draft',
+            //     'lwt',
+            //     'nrt',
+            //     'grt',
+            //     'dwt',
+            //     'death',
+            //     'breadth',
+            //     'length',
+            //     'mmsi',
+            //     'class',
+            //     'home_port'
+            // ]);
         });
     }
 };
