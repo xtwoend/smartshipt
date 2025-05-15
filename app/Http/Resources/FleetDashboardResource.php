2c4602880c54053340e48b7770df37fe490b5683
Abdul Hafidz Anshari <aditans88@gmail.com>
2025-05-15 08:48:36 +0700

    update

--- app/Http/Resources/FleetDashboardResource.php
+++ app/Http/Resources/FleetDashboardResource.php
@@ -15,15 +15,16 @@ class FleetDashboardResource extends JsonResource
     public function toArray($request)
     {
         $parrent = parent::toArray($request);
-        return array_merge($parrent, [
+        return $parrent;
+        // return array_merge($parrent, [
             // 'engine' => $this->engine(),
-            'engine_info' => $this->engine()?->information(),
+            // 'engine_info' => $this->engine()?->information(),
             // 'cargo_data' => $this->cargo(),
             // 'cargo_pump' => $this->cargo_pump(),
             // 'fuel' => $this->fuel(),
             // 'ballast' => $this->ballast(),
             // 'draft' => $this->draft(),
-            'cargo_pump_info' => $this->cargo_pump()?->information(),
-        ]);
+            // 'cargo_pump_info' => $this->cargo_pump()?->information(),
+        // ]);
     }
 }
